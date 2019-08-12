c5e2d26f080c98910cfaf007902674a64cb98d9d
Zartman, Andrew <andrew.zartman@dish.com>
2019-08-12 17:49:14 -0600

    updateing code and adding wireing diagram

--- vision999.py
+++ vision999.py
@@ -14,26 +14,28 @@ GPIO.setmode(GPIO.BCM) # Use GPIO numbering
 
 sleepAttentionTime = 2  #Give the user enough time to read the screen then look down at the leds
 sleepLowTime = .5       #Amount of time to have the LED Off in the loop
-sleepHighTime = 1       #Amount of time to have the LED On in the loop
+sleepHighTime = .5       #Amount of time to have the LED On in the loop
 
 ledPin1=4
-GPIO.setup(ledPin1, GPIO.OUT, initial=GPIO.LOW)
 ledPin2=5
-GPIO.setup(ledPin2, GPIO.OUT, initial=GPIO.LOW)
 ledPin3=6
+
+GPIO.setup(ledPin1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(ledPin2, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(ledPin3, GPIO.OUT, initial=GPIO.LOW) 
-int_blink = 0;
+
+programAbort = 0
+int_blink = 0;  # initialize the blink count to 0
 
 # Capturing the image for Google Vision.
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
-    camera.start_preview(fullscreen=False,window=(100,200,300,300))
-    camera.framerate = 15
-    camera.start_recording('video.h264', resize=(320, 240))
-    camera.wait_recording(5)
+    camera.start_preview(fullscreen=False,window=(100,200,500,500))
+    camera.awb_mode = 'fluorescent'
+    camera.image_effect = 'none'
+    sleep(5)
     camera.capture('dd.jpg', use_video_port=True)
-    camera.wait_recording(5)
-    camera.stop_recording()
+    camera.stop_preview()
 
 # Creating Request to send to Google Vision 
     with open('dd.jpg', 'rb') as image: 
@@ -64,61 +66,55 @@ with picamera.PiCamera() as camera:
        s = json_data['responses'][0]['textAnnotations'][0]['description']
        int_blink = int(s)
        print 'The number is >> ', int_blink
-       print '!!!!!!!! WATCH THE LED BLINK ', int_blink , ' TIMES !!!!!!!!'  
+       print '!!!!!!!! WATCH THE LED BLINK !!!!!!!!'  
      except:
        print("The number was not identified") 
-       print("!!!!!!!! PLEASE TRY AGAIN !!!!!!!!") 
+       print("!!!!!!!! PLEASE TRY AGAIN !!!!!!!!")
+       programAbort = 1   #If the number was not recognized, no need to continue
     camera.stop_preview()
     camera.close() 
 
+if programAbort == 0:   #Test if we should continue
 
-hundreds = divmod(int_blink,100)
-blinkHundreds = hundreds[0]
-#print 'blinkHundreds = ', blinkHundreds
+  hundreds = divmod(int_blink,100)
+  blinkHundreds = hundreds[0]
+  #print 'blinkHundreds = ', blinkHundreds
 
-tens= divmod(hundreds[1], 10)
-blinkTens = tens[0]
-#print 'blinkTens = ', blinkTens
+  tens= divmod(hundreds[1], 10)
+  blinkTens = tens[0]
+  #print 'blinkTens = ', blinkTens
 
-blinkOnes = tens[1]
-#print 'blinkOnes = ', blinkOnes
+  blinkOnes = tens[1]
+  #print 'blinkOnes = ', blinkOnes
 
-print 'Blinking the Hundreds ', blinkHundreds, 'times'
-if blinkHundreds > 0:
+  def blink( pin, blinkCount ):
+    # Blinks the LED defined by the pin blinkcount times 
+    for x in range(0,blinkCount):
+      GPIO.output(pin, GPIO.HIGH)
+      sleep(sleepHighTime) # Sleep for 1 second
+      GPIO.output(pin, GPIO.LOW)
+      sleep(sleepLowTime) # Sleep for 1 second
+
+  print 'Blinking the Hundreds ', blinkHundreds, 'times'
+  if blinkHundreds > 0:
  
-  # Wait 1 second before starting the LED Blink loop
-  sleep(1.5)
+    # Wait before starting the LED Blink loop
+    sleep(sleepAttentionTime)
 
-  # Blinks the LED as per the integer identified 
-  for x in range(0,blinkHundreds):
-     GPIO.output(ledPin3, GPIO.HIGH)
-     sleep(.5) # Sleep for 1 second
-     GPIO.output(ledPin3, GPIO.LOW)
-     sleep(.5) # Sleep for 1 second
+    blink(ledPin3, blinkHundreds)
 
 
-print ' Blinking the Tens ', blinkTens, 'times'
-if blinkTens > 0:
-  # Wait 1 second before starting the LED Blink loop
-  sleep(1.5)
+  print ' Blinking the Tens ', blinkTens, 'times'
+  if blinkTens > 0:
+    # Wait second before starting the LED Blink loop
+    sleep(sleepAttentionTime)
 
-  # Blinks the LED as per the integer identified 
-  for x in range(0,blinkTens):
-     GPIO.output(ledPin2, GPIO.HIGH)
-     sleep(.5) # Sleep for 1 second
-     GPIO.output(ledPin2, GPIO.LOW)
-     sleep(.5) # Sleep for 1 second
+    blink(ledPin2, blinkTens)
 
 
-
-print '  Blinking the Ones ', blinkOnes, 'times'
-if blinkOnes > 0:
+  print '  Blinking the Ones ', blinkOnes, 'times'
+  if blinkOnes > 0:
     # Wait 1 second before starting the LED Blink loop
-  sleep(1.5)
+    sleep(sleepAttentionTime)
 
-  # Blinks the LED as per the integer identified 
-  for x in range(0,blinkOnes):
-     GPIO.output(ledPin1, GPIO.HIGH)
-     sleep(.5) # Sleep for 1 second
-     GPIO.output(ledPin1, GPIO.LOW)
-     sleep(.5) # Sleep for 1 second
+    blink(ledPin1, blinkOnes)
